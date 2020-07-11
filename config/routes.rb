bc545ba5837938fc1e9421ff3f100aa92f34c4e9
Jack Cullen <jpcullen20@gmail.com>
2020-07-11 13:44:02 -0600

    Add merchant items endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants, except: [:new, :edit]
       resources :items, except: [:new, :edit]
+      namespace :merchants, path: "/merchants/:id" do
+        resources :items, only: [:index]
+      end
     end
   end
 
