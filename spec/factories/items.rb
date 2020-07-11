0a74462ce2b1e393f677d689c8985b1af4b5c15b
Jack Cullen <jpcullen20@gmail.com>
2020-07-11 12:35:54 -0600

    Set up items index endpoint spec

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name { Faker::Name .unique.name }
     description { Faker::TvShows::SiliconValley.motto }
-    unit_price { Faker::Number.between(from = 10.00, to = 999.00) }
+    unit_price { Faker::Number.between(from: 10.00, to: 999.00) }
     merchant
   end
 end
---
+++ spec/requests/api/v1/items/items_index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Item index endpoint" do
+  it "sends a list of items" do
+    create_list(:item, 3)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    items = JSON.parse(response.body)
+
+    expect(items['data'].count).to eq(3)
+    expect(items['data'].first['type']).to eq('merchant')
+  end
+end
