97653b62366c2ab744e5a4de6dfd8bce61c06603
Jack Cullen <jpcullen20@gmail.com>
2020-07-11 15:33:34 -0600

    Add search method to merchants class

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,22 @@
+require 'date'
+
 class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
   validates :name, presence: true
+
+  def self.search(params)
+    merchants = []
+    params.each do |attribute, value|
+      if attribute == 'created_at'
+        merchants << Merchant.where("created_at BETWEEN ? and ?", "%#{value.to_date.beginning_of_day}%", "%#{value.to_date.end_of_day}%")
+      elsif attribute == 'updated_at'
+        merchants << Merchant.where("updated_at BETWEEN ? and ?", "%#{value.to_date.beginning_of_day}%", "%#{value.to_date.end_of_day}%")
+      else
+        merchants << Merchant.where("#{attribute} like ?", "%#{value}%")
+      end
+    end
+    merchants.first.first
+  end
+
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Merchant, type: :model do
     it {should have_many(:invoices)}
     it {should have_many(:items)}
   end
+
+  describe 'methods' do
+    it "search" do
+      merchant1 = create(:merchant, name: "Walgreens Store")
+      merchant2 = create(:merchant)
+      params = {"name"=>"green", "created_at"=>"#{merchant1.created_at.to_s.split[0]}", "updated_at"=>"#{merchant1.updated_at.to_s.split[0]}"}
+      expect(Merchant.search(params)).to eq(merchant1)
+    end
+  end
 end
