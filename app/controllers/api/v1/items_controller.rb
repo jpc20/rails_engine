f2ca6df72e7416cd4893e6ab8810caba215bb08b
Jack Cullen <jpcullen20@gmail.com>
2020-07-11 13:00:56 -0600

    Add create item api endpoint

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,4 +8,15 @@ class Api::V1::ItemsController < ApplicationController
     item = Item.find(params[:id])
     render json: ItemSerializer.new(item).serializable_hash
   end
+
+  def create
+    item = Item.create(item_params)
+    render json: ItemSerializer.new(item).serializable_hash
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, except: [:new, :edit]
-      resources :items, only: [:index, :show]
+      resources :items, except: [:new, :edit, :destroy, :update]
     end
   end
 
