ce007a3a23e91306a3cbdddea1a145a9c94be3d2
Jack Cullen <jpcullen20@gmail.com>
2020-07-11 17:36:07 -0600

    Move search method to application record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,15 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.search(params)
+    objects = self.all
+    params.each do |attribute, value|
+      if ['created_at', 'updated_at'].include?(attribute)
+        objects = objects.where("#{attribute} BETWEEN ? and ?", "%#{value.to_date.beginning_of_day}%", "%#{value.to_date.end_of_day}%")
+      else
+        objects = objects.where("#{attribute} like ?", "%#{value.downcase}%")
+      end
+    end
+    objects.first
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,17 +4,4 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
   validates :name, presence: true
-
-  def self.search(params)
-    merchants = Merchant.all
-    params.each do |attribute, value|
-      if ['created_at', 'updated_at'].include?(attribute)
-        merchants = merchants.where("#{attribute} BETWEEN ? and ?", "%#{value.to_date.beginning_of_day}%", "%#{value.to_date.end_of_day}%")
-      else
-        merchants = merchants.where("#{attribute} like ?", "%#{value.downcase}%")
-      end
-    end
-    merchants.first
-  end
-
 end
