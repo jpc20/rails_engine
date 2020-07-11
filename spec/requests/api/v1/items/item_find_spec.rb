2f86412ead06ba221243196137d7e131ab8dffd0
Jack Cullen <jpcullen20@gmail.com>
2020-07-11 17:31:51 -0600

    Set up item find spec

---
+++ spec/requests/api/v1/items/item_find_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "Item find endpoints" do
+  it "single item find by name" do
+    item1 = create(:item, name: "Toothpaste")
+    item2 = create(:item)
+
+    get "/api/v1/items/find?name=OOTH"
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    item_json = JSON.parse(response.body)
+    expect(item_json['data']['id'].to_i).to eq(item1.id)
+  end
+
+  it "single item find by created_at and unit_price" do
+    item1 = create(:item, unit_price: 433.99)
+    item2 = create(:item)
+
+    get "/api/v1/items/find?created_at=#{item1.created_at}&unit_price=433"
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    item_json = JSON.parse(response.body)
+    expect(item_json['data']['id'].to_i).to eq(item1.id)
+  end
+end
