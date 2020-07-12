2ffcf651f226014107492413a7632cdfcf6fb18b
Jack Cullen <jpcullen20@gmail.com>
2020-07-12 09:17:28 -0600

    Refactor search method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,14 +4,20 @@ class ApplicationRecord < ActiveRecord::Base
   def self.search(params)
     objects = self.all
     params.each do |attribute, value|
-      if ['created_at', 'updated_at'].include?(attribute)
-        objects = objects.where("#{attribute} BETWEEN ? AND ?", "%#{value.to_date.beginning_of_day}%", "%#{value.to_date.end_of_day}%")
-      elsif attribute == "unit_price"
-        objects = objects.where("#{attribute} BETWEEN ? AND ?", (value.to_i - 1), (value.to_i + 1))
-      else
-        objects = objects.where("LOWER(#{attribute}) LIKE ?", "%#{value.downcase}%")
-      end
+      objects = filter_objects(attribute, value, objects)
     end
     objects
+  end
+
+  private
+
+  def self.filter_objects(attribute, value, objects)
+    if ['created_at', 'updated_at'].include?(attribute)
+      objects.where("#{attribute} BETWEEN ? AND ?", "%#{value.to_date.beginning_of_day}%", "%#{value.to_date.end_of_day}%")
+    elsif attribute == "unit_price"
+      objects.where("#{attribute} BETWEEN ? AND ?", (value.to_i - 1), (value.to_i + 1))
+    else
+      objects.where("LOWER(#{attribute}) LIKE ?", "%#{value.downcase}%")
+    end
   end
 end
